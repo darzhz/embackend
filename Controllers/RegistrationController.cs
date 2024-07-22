b5677dcafb3ebc92950e7aa4bd4ef837ffc42875
darzh <darzhz@protonmail.com>
2024-07-22 16:13:52 +0530

    backend complete

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -15,10 +15,15 @@ namespace embackend.Controllers{
             }
             try {
                 using var db = new RegisterContext();
-                db.users.Add(us);
-                await db.SaveChangesAsync();
-                var responce = new RegistrationResponce(us,"User registered successfully/");
-                return CreatedAtAction(nameof(RegisterUser),new {name= us.fname},responce);
+                var existingUser = await db.users.FirstOrDefaultAsync(u => u.email == us.email);
+                if(existingUser == null){
+                    db.users.Add(us);
+                    await db.SaveChangesAsync();
+                    var responce = new RegistrationResponce(us,"User registered successfully/");
+                    return CreatedAtAction(nameof(RegisterUser),new {name= us.fname},responce);
+                }else{
+                    return StatusCode(400, new { message = "User already Exists"});
+                }
             } catch (DbUpdateException ex){
                 return StatusCode(500, new { message = "An error occurred while saving the event.", error = ex.InnerException != null?ex.InnerException.Message:ex.Message });
             }
